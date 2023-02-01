e2beab6e63465e076108f4863bbd6eef1a5df1f8
Arhises <roma1039@gmail.com>
2023-02-01 22:07:23 +0200

    Sixth q added

---
+++ sixth_q/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Enter numbers between 100 and 999: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Last digit is: " + number % 10);
