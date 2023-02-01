951e90b4328869fd4d94e401f8a0f9268a02633e
Arhises <roma1039@gmail.com>
2023-02-01 22:22:39 +0200

    Eigth q added

---
+++ Eighth_q/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.WriteLine("Enter numbers between 10 and 99: ");
+int number = int.Parse(Console.ReadLine());
+
+int first = number % 10;
+int second = number / 10;
+
+if(first > second)
+{
+    Console.WriteLine("bigger digit is: " + first);
+}
+else
+{
+    Console.WriteLine("bigger digit is: " + second);
+}
