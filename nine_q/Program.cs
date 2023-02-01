364415074abf54543a2fcb6a3d6cbdbbdb2bdbaa
Arhises <roma1039@gmail.com>
2023-02-01 22:30:12 +0200

    nine q added

---
+++ nine_q/Program.cs
@@ -0,0 +1,10 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Enter numbers between 100 and 999: ");
+int number = int.Parse(Console.ReadLine());
+
+int firstDigit = number / 100;
+int lastDigit = number % 10;
+int newNumber = firstDigit * 10 + lastDigit;
+
+Console.WriteLine("The new number is: " + newNumber);
