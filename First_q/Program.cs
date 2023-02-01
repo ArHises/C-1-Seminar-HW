d78454b467adda6b19d9718c8cf0517688640872
Arhises <roma1039@gmail.com>
2023-02-01 21:07:29 +0200

    first q added

---
+++ First_q/Program.cs
@@ -0,0 +1,18 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("Enter first number to check: ");
+int first = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second number to check: ");
+int second = int.Parse(Console.ReadLine());
+
+if(first == second*second)
+{
+    Console.WriteLine("Match");
+}
+else
+{
+    Console.WriteLine("No match");
+}
+
+
