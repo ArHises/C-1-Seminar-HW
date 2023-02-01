048104915fc6c0058a99b5ba6c21b6456630be56
Arhises <roma1039@gmail.com>
2023-02-01 22:42:31 +0200

    Tenth q added

---
+++ Tenth_q/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Enter first numbers: ");
+int first = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second numbers: ");
+int second = int.Parse(Console.ReadLine());
+
+if(first % second == 0)
+{
+    Console.WriteLine("Its matches");
+}
+else
+{
+    Console.WriteLine("The remainder is: " + first % second);
+}
