cdb7bc7f60d511f429f0a98f7f2d110561c65d5e
Arhises <roma1039@gmail.com>
2023-02-01 21:57:22 +0200

    Fifth q added

---
+++ Fifth_q/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+int a = 0;
+
+while(a <= number)
+{
+    if (a % 2 == 0) 
+    {
+        Console.WriteLine(a);
+    }
+    a++;
+}
