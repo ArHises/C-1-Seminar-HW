66f5cc344ef572bbe5e7307a8c3ba7b628346c74
Arhises <roma1039@gmail.com>
2023-02-01 21:35:29 +0200

    Third added q

---
+++ Third_q/Program.cs
@@ -0,0 +1,23 @@
+﻿// Найти максимальное из трех чисел
+ Console.WriteLine("Enter first number: ");
+int first = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Enter second number: ");
+int second = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Enter third number: ");
+int third = int.Parse(Console.ReadLine());
+
+if(first > second && first > third)
+{
+    Console.WriteLine("Higher number is : " + first);
+}
+else if(second > first && second > third)
+{
+    Console.WriteLine("Higher number is : " + second);
+}
+else
+{
+    Console.WriteLine("Higher number is : " + third);
+}
+
