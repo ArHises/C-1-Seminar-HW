5fbd54ee0a67af34d6307c68883c8c87e45fbcdf
Arhises <roma1039@gmail.com>
2023-02-01 22:11:06 +0200

    Seventh q added

---
+++ Seventh_q/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Enter numbers between 100 and 999: ");
+int number = int.Parse(Console.ReadLine());
+number = number % 100 /10;
+
+Console.WriteLine("Last digit is: " + number);
