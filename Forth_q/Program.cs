5706a71b95321e8dc160556ce180a94c71654f92
Arhises <roma1039@gmail.com>
2023-02-01 21:43:30 +0200

    Forth q added

---
+++ Forth_q/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+ Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0) 
+{
+  Console.WriteLine(number + " is even");
+}
+else 
+{
+  Console.WriteLine(number + " is odd");
+}
