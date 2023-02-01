167f3121a431a30b306bb01373fdc1b295bc0626
Arhises <roma1039@gmail.com>
2023-02-01 22:50:25 +0200

    Ileventh q added

---
+++ Ileventh_q/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number / 100 != 0)
+{
+    Console.WriteLine("third digit is: "+ number / 100 % 10);
+}
+else
+{
+    Console.WriteLine("Third digit does not exist!");
+}
